1936d462f66cc03221ce9428ecb54dd8a3f4923b
jojun01835 <jojun09486@gmail.com>
2024-01-18 00:04:11 +0900

    Add files via upload

---
+++ reduxdemo2/src/components/Counter.js
@@ -0,0 +1,38 @@
+import { useSelector, useDispatch } from "react-redux";
+import classes from "./Counter.module.css";
+import { counterActions } from "../store/counterSlice";
+
+const Counter = () => {
+  const counter = useSelector((state) => state.counter.counter);
+  const dispatch = useDispatch();
+  const show = useSelector((state) => state.counter.showCounter);
+
+  const incrementHandler = () => {
+    dispatch(counterActions.increment());
+  };
+  const decrementHandler = () => {
+    dispatch(counterActions.decrement());
+  };
+
+  const increseHandler = () => {
+    dispatch(counterActions.increase(10));
+  };
+  const toggleCounterHandler = () => {
+    dispatch(counterActions.toggleCounter());
+  };
+
+  return (
+    <main className={classes.counter}>
+      <h1>Redux Counter</h1>
+      {show && <div className={classes.value}>{counter}</div>}
+      <div>
+        <button onClick={incrementHandler}>+</button>
+        <button onClick={increseHandler}>+5</button>
+        <button onClick={decrementHandler}>-</button>
+      </div>
+      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+    </main>
+  );
+};
+
+export default Counter;
---
+++ reduxdemo2/src/store/index.js
@@ -0,0 +1,15 @@
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "./counterSlice";
+import authReducer from "./auth";
+
+const store = configureStore({
+  reducer: {
+    counter: counterReducer,
+    auth: authReducer,
+  },
+});
+
+
+
+
+export default store;
