4c01f13a040a52862d1ae857d86e571a21fa916e
jojun01835 <jojun09486@gmail.com>
2024-01-17 23:55:02 +0900

    Add files via upload

---
+++ pre11/src/hooks/use-http.js
@@ -0,0 +1,38 @@
+import { useCallback, useState } from "react";
+
+const useHttp = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const sendReq =
+    ((useCallback = async (reqConfig, applyData) => {
+      setIsLoading(true);
+      setError(null);
+      try {
+        const response = await fetch(reqConfig.url, {
+          method: reqConfig.method ? reqConfig.method : "GET",
+          headers: reqConfig.headers ? reqConfig.headers : {},
+          body: reqConfig.body ? JSON.stringify(reqConfig.body) : null,
+        });
+
+        if (!response.ok) {
+          throw new Error("Request failed!");
+        }
+
+        const data = await response.json();
+        applyData(data);
+      } catch (err) {
+        setError(err.message || "Something went wrong!");
+      }
+      setIsLoading(false);
+    }),
+    []);
+
+  return {
+    isLoading: isLoading,
+    error: error,
+    sendReq: sendReq,
+  };
+};
+
+export default useHttp;
